ee6eee6f0e0b4cc690cc1bdefadb4d2c0665e0f5
Jorman <jorman.iisrael@me.com>
2021-11-02 12:54:31 -0400

    passwords

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const validPassword = dbUserData.checkPassword(req.body.password);
+
+
 
 // get all users
 router.get('/', (req, res) => {
@@ -47,11 +50,35 @@ router.post('/', (req, res) => {
     });
 });
 
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
+});
+
+
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id
     }
